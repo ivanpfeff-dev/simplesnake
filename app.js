1535b24d2bf6162854f83589814a99eb6bb1a8ed
Ivan <ivanpfeff@gmail.com>
2021-12-28 14:12:49 -0500

    Adding styles to support full screen canvas

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   console.log(`User Connected. HTTP Version: ${req.httpVersion} Method: ${req.method} URL: ${req.url} Remote Host: ${req.socket.remoteAddress}`);
-  res.writeHead(200, { 'content-type': 'text/html' });
   if(req.url === "/") {
+    res.writeHead(200, { 'content-type': 'text/html' });
     fs.createReadStream('index.html').pipe(res);
   }
   if (req.url === "/dist/main.js") {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
     fs.createReadStream('./dist/main.js').pipe(res); 
   }
   if (req.url === "/jquery.min.js") {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
     fs.createReadStream('./jquery.min.js').pipe(res); 
+  }
+  if (req.url === "/style.css") {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    fs.createReadStream('./style.css').pipe(res); 
   }
 });
 
