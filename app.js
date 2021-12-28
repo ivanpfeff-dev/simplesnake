76f7436dc10cbdd73d0524f567117dbb10718ba3
Ivan Pfefferkorn <ivanpfeff@gmail.com>
2021-12-27 19:39:46 -0500

    Fixing log line

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
+  console.log(`User Connected. HTTP Version: ${req.httpVersion} Method: ${req.method} URL: ${req.url} Remote Host: ${req.socket.remoteAddress}`);
   res.writeHead(200, { 'content-type': 'text/html' });
-  fs.createReadStream('index.html').pipe(res);
-    if (req.url === "/dist/main.js")
-    {
-     fs.createReadStream('/dist/main.js').pipe(res); 
-    }
-  console.log(
-  `User Connected. HTTP Version: ${req.httpVersion} Method:
-   ${req.method} URL: ${req.url} Remote Host: ${req.socket.remoteAddress}`);
+  if(req.url === "/") {
+    fs.createReadStream('index.html').pipe(res);
+  }
+  if (req.url === "/dist/main.js") {
+    fs.createReadStream('/dist/main.js').pipe(res); 
+  }
 });
-
 
 server.listen(3000);
 console.log("Listening on port 3000.");
