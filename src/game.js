5bb25a6ac0dab2c39bbb7051176dc09775fbaeb9
Ivan Pfefferkorn <ivanpfeff@gmail.com>
2021-12-28 17:13:04 -0500

    Adding game startup logic and a renderer loop

--- src/game.js
+++ src/game.js
@@ -1,9 +1,30 @@
 var Snake = require('./snake');
+var Renderer = require('./renderer');
 
-function Game (canvas) {
-    debugger;
-    var snake1 = new Snake(2, 2);
-    snake1.addSegment();
+function Game (window, canvas) {
+    var self = this;
+
+    window.requestAnimationFrame = (function(){
+        return  window.requestAnimationFrame       ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame    ||
+                function (callback) {
+                  window.setTimeout(callback, 1000 / 60);
+                };
+      })();
+
+    self.renderer = new Renderer();
+    self.renderer.setCanvas(canvas);
+
+    var cycle = function() {
+        console.log("Cycle called");
+        self.renderer.drawGrid(20, 20);
+        window.requestAnimationFrame(cycle);
+    };
+
+    self.start = function() {
+        window.requestAnimationFrame(cycle);
+    }
 }
 
 module.exports = Game;
--- src/renderer.js
+++ src/renderer.js
@@ -1,15 +1,13 @@
 function Renderer() {
     var self = this;
     self.canvas = null;
-    self.pixelWidth = 0;
-    self.pixelHeight = 0;
-
-    self.setCanvas = function(canvas, pixelWidth, pixelHeight){
-
+    
+    self.setCanvas = function(canvas){
+        self.canvas = canvas;
     };
 
     self.drawGrid = function (width, height){
-
+        
     };
 
     self.drawSegment = function(segment){
