18a6702e04f5a1fe2dd006621bb76483027fdb02
Otto <pfefferkornotto@gmail.com>
2021-12-27 20:27:54 -0400

    added snake and segment classes for game

---
+++ src/segment.js
@@ -0,0 +1,6 @@
+function Segment(x,y,orientation) {
+    var self = this;
+    self.x = x;
+    self.y = y;
+    self.orientation = orientation;
+}
--- src/snake.js
+++ src/snake.js
@@ -1,2 +1,12 @@
 function Snake() {
+    var self = this;
+    self.segments = [];
+    self.getLastSegment = function() {
+        return self.segments[self.segments.length-1]
+    }
+    self.addSegment = function() {
+        var lastSegment = self.getLastSegment();
+        self.segments.push();
+    }
+     
 }
