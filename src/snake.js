f9c7784c23559fda1ff5bae21ea936f25f25f277
Otto <pfefferkornotto@gmail.com>
2021-12-27 20:43:12 -0400

    added single-segment snake object to snake.js

--- src/snake.js
+++ src/snake.js
@@ -1,12 +1,16 @@
-function Snake() {
+var Direction = require('./direction');
+var Direction = require('./segment'); 
+function Snake(x,y) {
     var self = this;
     self.segments = [];
+    self.segments.push(new Segment(x,y,Direction.UP));
+
     self.getLastSegment = function() {
         return self.segments[self.segments.length-1]
     }
     self.addSegment = function() {
         var lastSegment = self.getLastSegment();
-        self.segments.push();
+        self.segments.push(Direction.getPreviousPosition(lastSegment));
     }
      
 }
