dbb5b86c12af59df0a37adb2b868ebbd2194cbdf
Otto <pfefferkornotto@gmail.com>
2021-12-28 17:19:08 -0400

    fixing merge

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
 function Renderer() {
     var self = this;
     self.canvas = null;
+<<<<<<< Updated upstream
     
     self.setCanvas = function(canvas){
         self.canvas = canvas;
@@ -8,6 +9,27 @@ function Renderer() {
 
     self.drawGrid = function (width, height){
         
+=======
+    self.context = null;
+
+    self.setCanvas = function(canvas){
+        self.canvas = canvas;
+        self.context = canvas.getContext("2d");
+    };
+
+    self.drawGrid = function (width, height){
+        for (var i = 0; i < self.canvas.width; i += width) {
+            self.context.moveTo(i,0);
+            self.context.lineTo(i, height);
+            self.context.stroke();
+        }
+
+        for (var h = 0; h < self.canvas.height; i += height) {
+            self.context.moveTo(0,i);
+            self.context.lineTo(width,i);
+            self.context.stroke();
+        }
+>>>>>>> Stashed changes
     };
 
     self.drawSegment = function(segment){
